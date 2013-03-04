a3f25c6beab0a60f7dd1ae4a698197152834e5f8
wahyu yudhianto <wahyu.yudhianto@gmail.com>
2013-03-04 14:09:01 -0500

    encrypted-password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,20 +1,21 @@
 class User < ActiveRecord::Base
-   attr_accessor :password
+   require 'digest/sha1'
    validates :password, :confirmation => true, :length => 6..8, :on => :create
 
-   require 'digest/sha1'
+   attr_accessor :password
+
    before_save :encrypt_password
    after_save :clear_password
 	
 	def encrypt_password
 		if password.present?
-			self.salt = Digest::SHA1.hexdigest("encrypt your password #{password}")
-			self.encrypt_password = Digest::SHA1.hexdigest("encrypt again with #{password}")
+			self.salt = Digest::SHA1.hexdigest("encrypt your password {password}")
+			self.encrypted_password = Digest::SHA1.hexdigest("Adding #{salt} to {password}")
 		end
 	end
 
 	def clear_password
 		self.password = nil
 	end
-   attr_accessible :username, :email, :password, :password_confirmation
+   attr_accessible :username, :email, :password, :password_confirmation, :encrypted_password
 end
